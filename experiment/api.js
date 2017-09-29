200361eecbdd80cee103866431ce507ff5d4825a
dietrich ayala <autonome@gmail.com>
2017-09-29 13:43:50 +0530

    isolate the batch op into a func

--- experiment/api.js
+++ experiment/api.js
@@ -1,5 +1,20 @@
 const {classes: Cc, interfaces: Ci, results: Cr, utils: Cu} = Components;
 
+function nativeShowOnlyTheseTabs(tabIds) {
+  // Convert tab ids into XUL tabs
+  var tabs = tabIds.map(function(tab) {
+    return context.extension.tabManager.get(tab.id, null);
+  });
+
+  // Get current browser window
+  var wm = Cc["@mozilla.org/appshell/window-mediator;1"]
+           .getService(Ci.nsIWindowMediator);
+  var browserWindow = wm.getMostRecentWindow("navigator:browser");
+
+  // Set visible tabs
+  var tabbrowser = browserWindow.gBrowser;
+  tabbrowser.showOnlyTheseTabs(tabs);
+}
 
 class API extends ExtensionAPI {
   getAPI(context) {
@@ -8,20 +23,7 @@ class API extends ExtensionAPI {
       // Note: the namespace (boilerplate must match the id in the install.rdf)
       showonlythesetabs: {
         async showOnlyTheseTabs(tabIds) {
-
-          // Convert WE tabs into XUL tabs
-          var tabs = tabIds.map(function(tab) {
-            return context.extension.tabManager.get(tab.id, null);
-          });
-
-          // Get current browser window
-					var wm = Cc["@mozilla.org/appshell/window-mediator;1"]
-                   .getService(Ci.nsIWindowMediator);
-					var browserWindow = wm.getMostRecentWindow("navigator:browser");
-
-          // Set visible tabs
-          var tabbrowser = browserWindow.gBrowser;
-          tabbrowser.showOnlyTheseTabs(tabs);
+          nativeShowOnlyTheseTabs(tabIds);
         }
       }
     };
